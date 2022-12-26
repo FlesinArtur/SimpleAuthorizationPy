1fb23b482df1f6eca473106b8b16f0e28e7ef6a8
Artur Flesin <flesinartur.com>
2022-12-27 00:02:06 +0200

    update to storage many users

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import os.path
 import json
 import pwinput
 SECURITY = 'database.json'
@@ -13,20 +14,48 @@ def password_validate():
             return user_password
 
 
-def file_writing(user_login, user_password):
-    database = {
-        "password": user_password,
-        "login": user_login
-    }
-    file = open(SECURITY, 'r+', encoding='utf-8')
-    line = file.readline()
-    if line != '':
-        file.write("\n")
+def create_storage(user_login, user_password):
+
+    if not os.path.exists(SECURITY):
+        file = open(SECURITY, 'w')
+        database = [{
+            "login": user_login,
+            "password": user_password
+        }]
+        json.dump(database, file)
+        file.close()
+
+    else:
+        database = get_data_from_storage()
+        add_user(database, user_login, user_password)
+
+
+def get_data_from_storage():
+
+    file = open(SECURITY, 'r')
+    database = json.load(file)
+    file.close()
+    return database
+
+
+def add_user(database, user_login, user_password):
+
+    database.append({
+            "login": user_login,
+            "password": user_password
+        })
+    file_writing(database)
+
+
+def file_writing(database):
+
+    file = open(SECURITY, 'w')
     json.dump(database, file)
     file.close()
 
 
 def registration():
+
     user_login = input('Введіть логін, який ви хочете використовувати: ')
     user_password = password_validate()
     while True:
@@ -35,22 +64,21 @@ def registration():
             print('Успішно!')
             break
         print('Ви ввели щось не так!')
-    file_writing(user_login=user_login, user_password=user_password)
+    create_storage(user_login=user_login, user_password=user_password)
 
 
 def authorization():
-    file = open(SECURITY, 'r', encoding='utf-8')
+
+    database = get_data_from_storage()
     temp = False
+
     while True:
         user_login = input('Введіть свій логін: ')
         user_password = pwinput.pwinput(prompt='Введіть свій пароль: ', mask='*')
 
-        while True:
-            line = file.readline()
-            if not line:
-                break
-            database = json.loads(line)
-            if database["login"] == user_login and database["password"] == user_password:
+        for user in database:
+
+            if user["login"] == user_login and user["password"] == user_password:
                 temp = True
                 print('Вітаю козаче, ти не забув хто ти!')
                 break
@@ -60,8 +88,6 @@ def authorization():
             print('Козаче, схоже ти забув хто ти, повтори спробу:(')
         else:
             break
-
-    file.close()
 
 
 def main():
